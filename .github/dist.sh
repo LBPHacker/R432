5b97182161233c89fec7cc465711ae21151a26be
Tamás Bálint Misius <lbphacker@gmail.com>
2026-07-13 22:05:27 +0200

    Fix dist script

--- .github/dist.sh
+++ .github/dist.sh
@@ -7,7 +7,7 @@ if [[ -d dist ]]; then
 	rm -r dist
 fi
 mkdir dist
-cp -r r4 modulepack.plot.conf dist/
+cp -r r4 r4.plot.modulepack dist/
 cd dist
 git submodule update --init
-luajit ../TPT-Script-Manager/modulepack.lua modulepack.plot.conf:../spaghetti/modulepack.plot.conf > r4plot.lua
+luajit ../TPT-Script-Manager/modulepack.lua r4.plot.modulepack:../spaghetti/spaghetti.plot.modulepack > r4plot.lua
